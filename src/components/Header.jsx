60840cabe9e2fb728bd9898ebc1975073783e1cb
User <user@MacBook-Air-de-Martinez.local>
2026-03-26 14:37:55 +0100

    Ajout du logo Kasa et peaufinage graphique desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,30 @@
 import { NavLink } from "react-router-dom";
 import "./Header.css";
+import logoKasa from "../assets/logo-kasa.png";
 
 function Header() {
   return (
     <header className="header">
-      <div className="header__logo">Kasa</div>
+      <NavLink to="/">
+        <img src={logoKasa} alt="Kasa" className="header__logo" />
+      </NavLink>
 
       <nav className="header__nav">
-        <NavLink to="/" className="header__link">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "header__link active" : "header__link"
+          }
+        >
           Accueil
         </NavLink>
 
-        <NavLink to="/about" className="header__link">
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "header__link active" : "header__link"
+          }
+        >
           A Propos
         </NavLink>
       </nav>
